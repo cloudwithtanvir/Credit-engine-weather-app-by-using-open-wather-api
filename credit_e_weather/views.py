f08209e18e8eae418bc065d92d72d897030780e0
tanvirewu <tanvirewu32@gmail.com>
2021-12-13 15:03:05 +0600

    updating the app & adding the openapi

---
+++ credit_e_weather/views.py
@@ -0,0 +1,38 @@
+import requests
+from django.shortcuts import render
+from .models import City
+from .forms import CityForm
+
+def index(request):
+    url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=imperial&appid=e7e8223d59f6f7c162af1daca66fd40d'
+
+    if request.method == 'POST':
+        form = CityForm(request.POST)
+        form.save()
+
+    form = CityForm()
+
+    cities = City.objects.all()
+
+    weather_data = []
+
+    for city in cities:
+
+        r = requests.get(url.format(city)).json()
+
+        city_weather = {
+            'city' : city.name,
+            'temperature' : r['main']['temp'],
+            'pressure' : r['main']['pressure'],
+            'humidity' : r['main']['humidity'],
+            
+
+            'description' : r['weather'][0]['description'],
+            'icon' : r['weather'][0]['icon'],
+            
+        }
+
+        weather_data.append(city_weather)
+
+    context = {'weather_data' : weather_data, 'form' : form}
+    return render(request, 'weather/weather.html', context)
